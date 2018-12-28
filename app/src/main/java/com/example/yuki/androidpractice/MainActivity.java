ccc158529fd740c324a744b454a90c587bf2ae25
ymtsk0131 <ymtsk0131@live.jp>
2018-12-28 21:51:30 +0900

    create onClick action

--- app/src/main/java/com/example/yuki/androidpractice/MainActivity.java
+++ app/src/main/java/com/example/yuki/androidpractice/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.yuki.androidpractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
     }
 }
